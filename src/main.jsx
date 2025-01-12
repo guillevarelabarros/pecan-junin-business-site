005c3f495d985ad4c153b630481e4d097226b849
Guillermo Ignacio Varela Barros <guillevarelabarros@gmail.com>
2025-01-12 20:21:16 -0300

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from "react-router-dom";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 import { PecanJuninApp } from './PecanJuninApp.jsx';
 import theme from './theme';
 
-import './styless.css'
+import './styless.css';
 import { ThemeProvider } from '@emotion/react';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-     <ThemeProvider theme={theme}>
+    <ThemeProvider theme={theme}>
       <BrowserRouter>
         <PecanJuninApp />
       </BrowserRouter>
     </ThemeProvider>
   </StrictMode>
-)
+);
